35b3674b85b073d6ef7e64e191ee0196eff24d45
Arnaud <hello@dotburo.org>
2018-03-12 23:35:17 +0100

    use minified html

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('CONTENT_DIR', ROOT_DIR .'storage/content/');
 define('CONTENT_EXT', '.md');
 define('LIB_DIR', ROOT_DIR .'pico/');
 define('PLUGINS_DIR', LIB_DIR .'plugins/');
-define('RESOURCES_DIR', ROOT_DIR .'resources/');
+define('RESOURCES_DIR', ROOT_DIR .'dist/');
 define('CACHE_DIR', LIB_DIR .'cache/');
 require_once(ROOT_DIR .'vendor/autoload.php');
 require_once(LIB_DIR .'pico.php');
